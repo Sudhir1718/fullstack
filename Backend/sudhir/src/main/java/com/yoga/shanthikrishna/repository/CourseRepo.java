56e661ac6c59e6be2a1d83a2dec43347c057c9b9
Sudhir1718 <119098804+Sudhir1718@users.noreply.github.com>
2024-03-05 15:38:11 +0530

    Delete Backend/sudhir directory

--- Backend/sudhir/src/main/java/com/yoga/shanthikrishna/repository/CourseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.yoga.shanthikrishna.repository;
-import com.yoga.shanthikrishna.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepo extends JpaRepository<Course,Long> {
-
-}
